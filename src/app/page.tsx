874ddaa38400df59dfabbf983ace39e0faf5ab75
Fabian Heeke <fbnheeke@googlemail.com>
2026-01-06 10:26:57 +0100

    Added Header and Board components to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
 'use client';
+import Header from '@/components/ui/header/header';
+import KanbanBoard from '@/features/kanban-board/KanbanBoard';
 
-export default function BoardLayout() {
-  return <></>;
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <KanbanBoard />
+    </>
+  );
 }

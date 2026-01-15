22c3f964a0d4899fe471aea75af92796ccd774a1
Fabian Heeke <fbnheeke@googlemail.com>
2026-01-15 10:25:11 +0100

    Added basic ui-elements to header

--- src/components/ui/header/header.tsx
+++ src/components/ui/header/header.tsx
@@ -1,5 +1,30 @@
-const Header = () => {
-  return <div className="h-24 w-full border-b border-black bg-amber-50"></div>;
+import classnames from 'classnames';
+import { FaGithub } from 'react-icons/fa';
+import { IoIosSettings } from 'react-icons/io';
+
+interface HeaderProps {
+  maxContentWidthClassName: string;
+}
+
+const Header = ({ maxContentWidthClassName }: HeaderProps) => {
+  console.log(maxContentWidthClassName);
+  return (
+    <div className="h-20 w-full bg-gray-100">
+      <div
+        className={classnames(
+          'mx-auto flex h-full items-center justify-between',
+          maxContentWidthClassName
+        )}
+      >
+        <button className="flex cursor-pointer items-center gap-2 rounded border border-black p-2">
+          Projekt auf GitHub
+          <FaGithub />
+        </button>
+
+        <IoIosSettings />
+      </div>
+    </div>
+  );
 };
 
 export default Header;

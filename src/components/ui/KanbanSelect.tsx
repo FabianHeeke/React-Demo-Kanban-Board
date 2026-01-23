fc899ea9cc13ad3403fa08486cadd2fdf483b194
Fabian Heeke <fbnheeke@googlemail.com>
2026-01-23 11:50:31 +0100

    Updated KanbanSelect to always show all options with a checkmark for selected

--- src/components/ui/KanbanSelect.tsx
+++ src/components/ui/KanbanSelect.tsx
@@ -2,18 +2,14 @@ import { KanbanSelectOption } from '@/features/kanban-board/types/KanbanSelectOp
 import { useState, useCallback } from 'react';
 import classnames from 'classnames';
 import KanbanIcon from './KanbanIcon';
+import { FaCheck } from 'react-icons/fa6';
 
 interface KanbanSelectProps {
   options: KanbanSelectOption[];
   onValueSelect: (selectedValue: KanbanSelectOption['value']) => void;
-  prefixIcon?: React.FC;
 }
 
-const KanbanSelect = ({
-  options,
-  onValueSelect,
-  prefixIcon,
-}: KanbanSelectProps) => {
+const KanbanSelect = ({ options, onValueSelect }: KanbanSelectProps) => {
   const [selectedOption, setSelectedOption] = useState<KanbanSelectOption>(
     options[0]
   );
@@ -29,44 +25,42 @@ const KanbanSelect = ({
   );
 
   return (
-    <div
-      onClick={() => setIsSelectDropdownVisible(!isSelectDropdownVisible)}
-      className={classnames(
-        'hover:border-custom-dark absolute top-0 z-20 max-w-fit cursor-pointer rounded border px-2 py-1 transition-colors hover:bg-white',
-        {
-          'border-custom-dark bg-white': isSelectDropdownVisible,
-          'border-transparent': !isSelectDropdownVisible,
-        }
-      )}
-    >
-      <div className="relative flex items-center gap-1">
-        {prefixIcon && <KanbanIcon icon={prefixIcon} size={15} />}
-        <p className="hover:underline">{selectedOption.displayName}</p>
-      </div>
+    <div className="relative">
       <div
+        onClick={() => setIsSelectDropdownVisible(!isSelectDropdownVisible)}
         className={classnames(
-          'grid grid-rows-[0fr] opacity-0 transition-[grid-template-rows] duration-200 ease-in-out',
+          'hover:border-custom-dark absolute top-0 z-20 max-w-fit cursor-pointer rounded border px-2 py-1 transition-colors hover:bg-white',
           {
-            'grid-rows-[1fr] opacity-100': isSelectDropdownVisible,
+            'border-custom-dark bg-white': isSelectDropdownVisible,
+            'border-transparent': !isSelectDropdownVisible,
           }
         )}
       >
-        <div className="mt-1 flex w-max min-w-full flex-col gap-1 overflow-hidden">
-          {options
-            .filter((option) => option !== selectedOption)
-            .map((option, index) => (
+        <div className="flex items-center gap-1">
+          <p className="hover:underline">{selectedOption.displayName}</p>
+        </div>
+        <div
+          className={classnames(
+            'grid grid-rows-[0fr] opacity-0 transition-[grid-template-rows] duration-200 ease-in-out',
+            {
+              'grid-rows-[1fr] opacity-100': isSelectDropdownVisible,
+            }
+          )}
+        >
+          <div
+            className={classnames(
+              'border-custom-dark flex w-max min-w-full flex-col gap-1 overflow-hidden border-t',
+              {
+                'mt-2 pt-2': isSelectDropdownVisible,
+              }
+            )}
+          >
+            {options.map((option, index) => (
               <div
                 key={`sort-selct-option-${index}-${option.value} `}
-                className="group flex gap-1"
+                className="group flex items-center gap-2"
                 onClick={() => handleValueSelect(option)}
               >
-                {prefixIcon && (
-                  <KanbanIcon
-                    icon={prefixIcon}
-                    size={15}
-                    className="invisible"
-                  />
-                )}
                 <p
                   className={classnames('group-hover:underline', {
                     hidden: !isSelectDropdownVisible,
@@ -74,8 +68,12 @@ const KanbanSelect = ({
                 >
                   {option.displayName}
                 </p>
+                {option.displayName === selectedOption.displayName ? (
+                  <KanbanIcon icon={FaCheck} size={15} />
+                ) : null}
               </div>
             ))}
+          </div>
         </div>
       </div>
     </div>

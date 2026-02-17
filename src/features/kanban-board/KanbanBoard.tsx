94170aed4a1bd3a30e46b63b6992f8f2518e1847
Fabian Heeke <Fa.Heeke@googlemail.com>
2026-02-17 11:40:00 +0100

    Changed icons to be wrapped with new icon component

--- src/features/kanban-board/KanbanBoard.tsx
+++ src/features/kanban-board/KanbanBoard.tsx
@@ -4,6 +4,7 @@ import useBoardStore from './hooks/useBoardStore';
 import { DndContext } from '@dnd-kit/core';
 import { PiSortAscending } from 'react-icons/pi';
 import { FaPlus } from 'react-icons/fa6';
+import KanbanIcon from '@/components/ui/KanbanIcon';
 
 const KanbanBoard = () => {
   const { columns } = useBoardStore();
@@ -13,8 +14,8 @@ const KanbanBoard = () => {
   return (
     <div className="mt-8 flex max-w-full flex-col gap-4">
       <div className="flex justify-between px-2">
-        <PiSortAscending />
-        <FaPlus />
+        <KanbanIcon icon={PiSortAscending} />
+        <KanbanIcon icon={FaPlus} />
       </div>
       <DndContext onDragEnd={onCardDragEnd}>
         <div className="flex h-full max-h-screen min-h-[50vh] justify-center gap-4">

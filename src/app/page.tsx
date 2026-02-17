7c1a03ff89b4cee148e6ab8d6e0337abcf1f155b
Fabian Heeke <Fa.Heeke@googlemail.com>
2026-02-17 11:40:00 +0100

    Added basic ui-elements to header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 'use client';
 import Header from '@/components/ui/header/header';
 import KanbanBoard from '@/features/kanban-board/KanbanBoard';
+import classnames from 'classnames';
 
 export default function Home() {
+  const maxContentWidthClassName = 'max-w-4xl';
   return (
     <>
-      <Header />
-      <div className="relative mx-auto max-w-4xl">
+      <Header maxContentWidthClassName={maxContentWidthClassName} />
+      <div className={classnames('relative mx-auto', maxContentWidthClassName)}>
         <KanbanBoard />
       </div>
     </>

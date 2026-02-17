78350a4dd1011f421818e12354ac0f730499c100
Fabian Heeke <Fa.Heeke@googlemail.com>
2026-02-17 11:40:00 +0100

    Updated BoardColumn.tsx to include new TaskCard component

--- src/features/kanban-board/components/BoardColumn.tsx
+++ src/features/kanban-board/components/BoardColumn.tsx
@@ -1,10 +1,11 @@
 import Column from '../interfaces/Column.interface';
+import { TaskCard } from './TaskCard';
 
-interface ColumnProps {
+interface BoardColumnProps {
   column: Column;
 }
 
-export const BoardColumn = ({ column }: ColumnProps) => {
+export const BoardColumn = ({ column }: BoardColumnProps) => {
   const _renderTasks = () => {
     if (column.tasks.length === 0) {
       return <div className="text-sm text-gray-500 italic">No tasks</div>;
@@ -13,14 +14,14 @@ export const BoardColumn = ({ column }: ColumnProps) => {
     return (
       <>
         {column.tasks.map((task, index) => (
-          <p key={index}>{task}</p>
+          <TaskCard key={`task-${column.id}-${index}`} task={task} />
         ))}
       </>
     );
   };
 
   return (
-    <div className="flex w-72 flex-col rounded-lg bg-gray-100 p-4">
+    <div className="flex w-72 flex-col overflow-hidden rounded-lg bg-gray-100 p-4">
       <h3 className="mb-4 font-bold">{column.name}</h3>
       <div className="flex flex-col gap-2">{_renderTasks()}</div>
     </div>

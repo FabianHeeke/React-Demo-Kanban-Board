66c04c9f1157271d880d16cc0a0de140087dd19b
Fabian Heeke <Fa.Heeke@googlemail.com>
2026-02-17 11:40:00 +0100

    Updated styling in KanbanBoard.tsx

--- src/features/kanban-board/KanbanBoard.tsx
+++ src/features/kanban-board/KanbanBoard.tsx
@@ -4,10 +4,12 @@ import useBoardStore from './store/useBoardStore';
 const KanbanBoard = () => {
   const { columns } = useBoardStore();
   return (
-    <div className="flex h-full min-h-screen gap-4 overflow-x-auto bg-gray-50 p-8">
-      {columns.map((column, index) => (
-        <BoardColumn key={index} column={column} />
-      ))}
+    <div className="p-8">
+      <div className="flex h-full max-h-screen min-h-[50vh] gap-4 overflow-x-auto bg-gray-50">
+        {columns.map((column, index) => (
+          <BoardColumn key={index} column={column} />
+        ))}
+      </div>
     </div>
   );
 };

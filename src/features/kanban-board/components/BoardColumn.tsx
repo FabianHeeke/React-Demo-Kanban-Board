bc56cd9e8da0ac28de78e7ae941209986f6906b8
Fabian Heeke <Fa.Heeke@googlemail.com>
2026-02-17 11:40:00 +0100

    Added dnd-kit components to markup and connected the drag logic

--- src/features/kanban-board/components/BoardColumn.tsx
+++ src/features/kanban-board/components/BoardColumn.tsx
@@ -1,11 +1,20 @@
 import Column from '../interfaces/Column.interface';
 import { TaskCard } from './TaskCard';
+import classnames from 'classnames';
+import { useDroppable } from '@dnd-kit/core';
 
 interface BoardColumnProps {
   column: Column;
 }
 
 export const BoardColumn = ({ column }: BoardColumnProps) => {
+  const { isOver, setNodeRef } = useDroppable({
+    id: column.id,
+  });
+  const styleDrop = {
+    background: isOver ? '' : undefined,
+  };
+
   const _renderTasks = () => {
     if (column.tasks.length === 0) {
       return <div className="text-sm text-gray-500 italic">No tasks</div>;
@@ -14,14 +23,26 @@ export const BoardColumn = ({ column }: BoardColumnProps) => {
     return (
       <>
         {column.tasks.map((task, index) => (
-          <TaskCard key={`task-${column.id}-${index}`} task={task} />
+          // Task Card is draggable
+          <TaskCard
+            key={`task-${column.id}-${index}`}
+            task={task}
+            columnId={column.id}
+          />
         ))}
       </>
     );
   };
 
   return (
-    <div className="flex w-72 flex-col overflow-hidden rounded-lg bg-gray-100 p-4">
+    <div
+      ref={setNodeRef}
+      style={styleDrop}
+      className={classnames(
+        'flex w-72 flex-col rounded-lg border border-black p-4',
+        { 'bg-gray-50': isOver }
+      )}
+    >
       <h3 className="mb-4 font-bold">{column.name}</h3>
       <div className="flex flex-col gap-2">{_renderTasks()}</div>
     </div>

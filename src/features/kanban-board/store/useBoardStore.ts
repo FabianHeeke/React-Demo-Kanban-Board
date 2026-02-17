d74b22d0e6460fac100e4e05829f76d4fb5c926c
Fabian Heeke <Fa.Heeke@googlemail.com>
2026-02-17 11:40:00 +0100

    Added column interface in store

--- src/features/kanban-board/store/useBoardStore.ts
+++ src/features/kanban-board/store/useBoardStore.ts
@@ -1,11 +1,16 @@
 import { create } from 'zustand';
+import Column from '../interfaces/Column.interface';
 
 interface BoardState {
-  columns: string[];
+  columns: Column[];
   tasks: [];
 }
 
-const initialColumns = ['Todo', 'In progress', 'Done'];
+const initialColumns: Column[] = [
+  { name: 'Todo', tasks: [] },
+  { name: 'In progress', tasks: [] },
+  { name: 'Done', tasks: [] },
+];
 
 const useBoardStore = create<BoardState>(() => ({
   columns: initialColumns,

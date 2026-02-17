534f2693f51bca8f19c9c456fa0dce5036a24670
Fabian Heeke <Fa.Heeke@googlemail.com>
2026-02-17 11:40:02 +0100

    Addded new component KanbanCheckboxSelect

---
+++ src/components/ui/KanbanCheckboxSelect.tsx
@@ -0,0 +1,55 @@
+import { KanbanSelectOption } from '@/features/kanban-board/types/KanbanSelectOption.type';
+import { useState } from 'react';
+import classnames from 'classnames';
+import classNames from 'classnames';
+
+interface KanbanCheckboxSelectProps {
+  options: KanbanSelectOption[];
+  onValueSelect: (selectedValue: KanbanSelectOption['value']) => void;
+  preSelectedValue?: KanbanSelectOption;
+}
+
+const KanbanCheckboxSelect = ({
+  options,
+  onValueSelect,
+}: KanbanCheckboxSelectProps) => {
+  const [selectedOption, setSelectedOption] = useState<KanbanSelectOption>(
+    options[0]
+  );
+
+  const handleValueSelect = (selectedOption: KanbanSelectOption) => {
+    setSelectedOption(selectedOption);
+    onValueSelect(selectedOption.value);
+  };
+
+  return (
+    <div className={classnames('flex gap-5')}>
+      {options.map((option, index) => (
+        <div
+          key={`sort-selct-option-${index}-${option.value} `}
+          className="group flex cursor-pointer items-center gap-2"
+          onClick={() => handleValueSelect(option)}
+        >
+          <span
+            className={classnames('group-hover:font-semibold', {
+              'font-semibold':
+                option.displayName === selectedOption.displayName,
+            })}
+          >
+            {option.displayName}
+          </span>
+          <div className="border-custom-dark lative box-border h-4 w-4 rounded border p-px">
+            <div
+              className={classNames('h-full w-full rounded', {
+                'bg-custom-dark':
+                  option.displayName === selectedOption.displayName,
+              })}
+            ></div>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default KanbanCheckboxSelect;

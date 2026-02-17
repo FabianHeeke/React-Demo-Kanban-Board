5f99e1813b15090bef891c389978b1f011d8a048
Fabian Heeke <Fa.Heeke@googlemail.com>
2026-02-17 11:40:00 +0100

    Added store to KanbanBoard component

--- src/features/kanban-board/KanbanBoard.tsx
+++ src/features/kanban-board/KanbanBoard.tsx
@@ -1,5 +1,14 @@
+import useBoardStore from './store/useBoardStore';
+
 const KanbanBoard = () => {
-  return <></>;
+  const { columns } = useBoardStore();
+  return (
+    <div className="flex h-full min-h-screen gap-4 overflow-x-auto bg-gray-50 p-8">
+      {columns.map((columnName, index) => (
+        <div key={index}>{columnName}</div>
+      ))}
+    </div>
+  );
 };
 
 export default KanbanBoard;
